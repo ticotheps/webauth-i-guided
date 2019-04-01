05322cf06c9fb300a73846970c6af684f6bb5635
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-01 17:14:31 -0400

    Modifies the restricted() middleware function to refer to express-session instead of having to verify the user's credentials with each request to that endpoint

--- index.js
+++ index.js
@@ -72,27 +72,38 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-function restricted(req, res, next) {
-  const { username, password } = req.headers;
 
-  if (username && password) {
-    Users.findBy({ username })
-    .first()
-    .then(user => {
-      // check that passwords match
-      if (user && bcrypt.compareSync(password, user.password)) {
-        next();
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ message: 'Ran into an unexpected error' });
-    });
+function restricted(req, res, next) {
+
+  if (req.session && req.session.username) {
+    next();
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(401).json({ message: 'You shall not pass!' });
   }
 }
+
+// BELOW CODE IS NO LONGER NEEDED BECAUSE OF COOKIES
+// function restricted(req, res, next) {
+//   const { username, password } = req.headers;
+
+//   if (username && password) {
+//     Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//       // check that passwords match
+//       if (user && bcrypt.compareSync(password, user.password)) {
+//         next();
+//       } else {
+//         res.status(401).json({ message: 'Invalid Credentials' });
+//       }
+//     })
+//     .catch(error => {
+//       res.status(500).json({ message: 'Ran into an unexpected error' });
+//     });
+//   } else {
+//     res.status(400).json({ message: 'No credentials provided' });
+//   }
+// }
 
 // if using axios, the syntax will look like this: 
 // "axios.get(url, { headers: { username, password } })"
