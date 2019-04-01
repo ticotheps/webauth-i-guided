a0f65e248ee359c3a2f9ac5c8dbf607930f93aca
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-01 14:14:11 -0400

    Reverted changes to two previous commits ago

--- index.js
+++ index.js
@@ -54,6 +54,31 @@ server.post('/api/login', (req, res) => {
     });
 });
 
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+      // check that passwords match
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Ran into an unexpected error' });
+    });
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
+}
+
+// if using axios, the syntax will look like this: 
+// "axios.get(url, { headers: { username, password } })"
+
 
 // Goal: protect this route! Only authenticated users should see it!
 // If using postman, a user should NOT be able to return the list 
@@ -81,31 +106,6 @@ server.get('/api/users', restricted, (req, res) => {
 //     res.send(err)
 //   }
 // });
-
-function restricted(req, res, next) {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-    .first()
-    .then(user => {
-      // check that passwords match
-      if (user && bcrypt.compareSync(password, user.password)) {
-        next();
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ message: 'Ran into an unexpected error' });
-    });
-  } else {
-    res.status(400).json({ message: 'No credentials provided' });
-  }
-}
-
-// if using axios, the syntax will look like this: 
-// "axios.get(url, { headers: { username, password } })"
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
